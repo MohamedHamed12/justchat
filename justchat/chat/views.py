1e3f40100b49e6954cd4765fe941f349cd83fdc3
MohamedHamed12 <mh1779371@gmail.com>
2023-03-06 19:15:47 +0200

    create_websocket

---
+++ justchat/chat/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+
+
+# chat/views.py
+
+
+def index(request):
+    return render(request, "chat/index.html")
+
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
